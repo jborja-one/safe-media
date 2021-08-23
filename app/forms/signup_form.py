94b8b53946b17441c82b6b0809fae3e925f5c75c
jborja-one <jborja.one@gmail.com>
2021-08-23 17:48:54 -0600

    fixed user form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -14,14 +14,14 @@ def user_exists(form, field):
 
 def username_exists(form, field):
     # Checking if username is already in use
-    username = field.data
-    user = User.query.filter(User.username == username).first()
+    email = field.data
+    user = User.query.filter(User.email == email).first()
     if user:
         raise ValidationError('Username is already in use.')
 
 
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('Email', validators=[DataRequired(), user_exists])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
